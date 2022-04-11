a152723a54f0b9ccb902c8bb476725aad221016f
YuliaVenglovska <juliavenglovska@gmail.com>
2022-04-11 14:46:34 +0300

    Added 2 task@

---
+++ TaskMvc/TaskMvc/Controllers/HomeControllerJulia.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskMvc.Models;
+
+namespace TaskMvc.Controllers
+{
+    public class HomeControllerJulia : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(PersonViewModel person)
+        {
+            return View("Name", person);
+        }
+    }
+}
---
+++ TaskMvc/TaskMvc/Infrastructure/JuliaControllerModelConvention.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace TaskMvc.Infrastructure
+{
+    public class JuliaControllerModelConvention : IControllerModelConvention
+    {
+        public void Apply(ControllerModel controller)
+        {
+            controller.ControllerName = controller.ControllerName.Replace("ControllerJulia", "");
+        }
+    }
+}
